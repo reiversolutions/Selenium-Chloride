670418360fea9b1e2400aec4cf843bac5788bb11
Daniel Charlton <charlton.dan@reiversolutions.co.uk>
2015-07-30 10:55:00 +0100

    Class to css selector

---
+++ Selenium Chloride/Public/IWebElementObjects/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Selenium_Chloride.Public.IWebElementObjects.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Takes a list of class names on an element and 
+        /// converts them to a css selector
+        /// </summary>
+        /// <param name="classNames">Classes on an element</param>
+        /// <returns>Css selector</returns>
+        public static string ClassListToCssSelector(this string classNames)
+        {
+            return "." + classNames.Replace(' ', '.');
+        }
+    }
+}
